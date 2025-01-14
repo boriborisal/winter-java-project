283ca7776bb75772274280d90b92a1afb9285a04
Soeun Park <soeunpark0806@gmail.com>
2025-01-15 00:32:58 +0900

    changejanfiftin

--- winterProject2/예제2_1.java
+++ winterProject2/예제2_1.java
@@ -27,9 +27,9 @@ public class 예제2_1 {
     //-> 참고사이트: https://e1jong.tistory.com/30
     //-> 참고사이트: https://eunplay.tistory.com/118
     public static int[] plus(int[] array1, int[] array2) { //plus 함수 
-        int[] mergedArray = new int[array1.length + array2.length];
-        System.arraycopy(array1, 0, mergedArray, 0, array1.length);
-        System.arraycopy(array2, 0, mergedArray, array1.length, array2.length);
-        return mergedArray;
+        int[] mergeArray = new int[array1.length + array2.length];
+        System.arraycopy(array1, 0, mergeArray, 0, array1.length);
+        System.arraycopy(array2, 0, mergeArray, array1.length, array2.length);
+        return mergeArray;
     }        
 }
